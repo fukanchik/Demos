79ee565a2844c8a62927f17b474ea0882d69c6d7
Sergey Fukanchik <fukanchik@gmail.com>
2023-08-13 16:48:24 -0400

    atomic, volatile, mfence, none

--- dekker/dekker.cpp
+++ dekker/dekker.cpp
@@ -1,7 +1,18 @@
-/* Store buffering affects https://en.wikipedia.org/wiki/Dekker%27s_algorithm */
+/* Store buffering affects https://en.wikipedia.org/wiki/Dekker%27s_algorithm 
+TODO: lfence, sfence, memory_order_acq_rel, memory_order_seq_cst, memory_order_relaxed
+https://stackoverflow.com/questions/27595595/when-are-x86-lfence-sfence-and-mfence-instructions-required
+*/
 #include <iostream>
 #include <atomic>
 #include <thread>
+
+/*
+1 - atomic
+2 - volatile
+3 - mfence
+4 - none
+*/
+#define CODE 4
 
 int
 main(int argc, char *argv[])
@@ -10,24 +21,36 @@ main(int argc, char *argv[])
 
   for(int i=0; i < 200000; ++i)
     {
-      //std::atomic_int x{0};
-      //std::atomic_int y{0};
+#if CODE==1
+      std::atomic_int x{0};
+      std::atomic_int y{0};
+#elif CODE==2
+      volatile int x{0};
+      volatile int y{0};
+#else
       int x{0};
       int y{0};
+#endif
 
       std::thread g1([&]{
                        x=1;
+#if CODE==3
+asm volatile("mfence":::"memory");
+#endif
                        if(y==0)x=2;
                      });
       std::thread g2([&]{
                        y=1;
+#if CODE==3
+asm volatile("mfence":::"memory");
+#endif
                        if(x==0)y=2;
                      });
       g1.join();
       g2.join();
-      if(x==2 && y==2) ++viol;
+      if(x==2 && y==2) ++violations;
     }
-  std::cout << "N="<< viol<< std::endl;
+  std::cout << "N="<< violations << std::endl;
   return 0;
 }
 
