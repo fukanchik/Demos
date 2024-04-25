2b1c5c53793f973e4f2e0c15dc268c5cc22068f0
Sergey Fukanchik <s.fukanchik@postgrespro.ru>
2024-04-25 17:00:50 +0300

    Adding leap year demo

---
+++ leap.c
@@ -0,0 +1,22 @@
+bool isleap_fast(int year) {
+    if (year %4 == 0) {
+        if (year %100 == 0) {
+            if (year %400 == 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+int isleap_branchless(int year)
+{
+    int div4 = !(year % 4);
+    int div100 = !(year % 100);
+    int div400 = !(year % 400);
+    
+    return (div4 & !div100) | div400;
+}
+
