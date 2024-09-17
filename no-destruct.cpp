48c05fbc807facb58d53cf528b82a9dd3f8110f9
Sergey Fukanchik <s.fukanchik@postgrespro.ru>
2024-09-17 12:33:20 +0300

    Add auto storage class

--- no-destruct.cpp
+++ no-destruct.cpp
@@ -1,20 +1,20 @@
 // If no matching handler is found, the function std::terminate() is called;
 // whether or not the stack is unwound before this call to std::terminate()
 // is implementation-defined (15.5.1).
-#include <iostream>
 #include <exception>
+#include <iostream>
 #include <memory>
 
-class X
-{
+class X {
+  int x;
+
 public:
-X(){std::cout << "X::X()" << std::endl;}
-~X(){std::cout << "X::~X()" << std::endl;}
+  X(int a) : x(a) { std::cout << "X::X(" << x << ")" << std::endl; }
+  ~X() { std::cout << "X::~X(" << x << ")" << std::endl; }
 };
 
-int main()
-{
- std::unique_ptr p = std::make_unique<X>();
-throw std::exception();
-return 0;
+int main() {
+  X x{42};
+  std::unique_ptr p = std::make_unique<X>(11);
+  throw std::exception();
 }
