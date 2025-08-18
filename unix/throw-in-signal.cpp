74c574fee9389d482296e0588000aa3266997e27
Sergey Fukanchik <s.fukanchik@postgrespro.ru>
2025-08-18 19:27:41 +0300

    Better demo for heap corruption in improper signal handler

--- unix/throw-in-signal.cpp
+++ unix/throw-in-signal.cpp
@@ -1,28 +1,62 @@
-/* Throw exception from within a signal handler. */
-#include <cstring>
-#include <iostream>
+/*
+  Throw exception from within a signal handler.
+  This causes heap corruption b/c of allocated exception block.
+  Compile with optimization to get the error faster.
+*/
 #include <signal.h>
 #include <unistd.h>
 
-void signal_handler(int n) {
-  ::write(1, "Signal handler\n", 6);
-  throw 1;
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+static void
+signal_handler(int n)
+{
+	if (::write(1, "Signal handler\n", 6) == -1)
+		abort();
+	throw 1;
 }
 
-int main() {
+static void
+setup_signal()
+{
+	struct sigaction sig;
+	memset(&sig, 0, sizeof(struct sigaction));
+	sig.sa_handler = signal_handler;
+	sigemptyset(&sig.sa_mask);
+	sig.sa_flags = 0;
 
-  struct sigaction sigIntHandler;
-  memset(&sigIntHandler, 0, sizeof(struct sigaction));
-  sigIntHandler.sa_handler = signal_handler;
-  sigemptyset(&sigIntHandler.sa_mask);
-  sigIntHandler.sa_flags = 0;
+	sigaction(SIGINT, &sig, NULL);
+}
 
-  sigaction(SIGINT, &sigIntHandler, NULL);
+int
+main(int argc, char *argv[])
+{
+	(void)argv;
+	bool need_allocate = argc > 1;
+	setup_signal();
 
-  try {
-    pause();
-  } catch (int v) {
-    std::cerr << "VAL " << v << std::endl;
-  }
-  return 0;
+	try
+	{
+		if (need_allocate)
+		{
+			/* This conflicts with __cxa_allocate_exception which corrupts heap.
+			 */
+			while (true)
+			{
+				char *p = new char[1];
+				delete[] p;
+			}
+		}
+		else
+		{
+			pause();
+		}
+	}
+	catch (int v)
+	{
+		std::cerr << "VAL " << v << std::endl;
+	}
+	return 0;
 }
