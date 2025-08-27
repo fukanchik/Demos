0159ba6bc325476ac3126398e59ad563dd6867b2
Sergey Fukanchik <s.fukanchik@postgrespro.ru>
2025-08-27 16:38:26 +0300

    stderr is unbuffered

--- unix/printf-twice.c
+++ unix/printf-twice.c
@@ -1,3 +1,4 @@
+/* man 3 stderr: The stream stderr is unbuffered.  The stream stdout is line-buffered when it points to a terminal. */
 #include <stdio.h>
 #include <unistd.h>
 
@@ -5,8 +6,10 @@ int
 main()
 {
 	printf("AAA");
+    fprintf(stderr, "BBB");
 	fork();
 	printf("\n");
+    fprintf(stderr, "\n");
 	// fflush(stdout);
 	return 0;
 }
