a7c8efaf02f07a60a32556d34aa8523a331b90a4
Sergey Fukanchik <s.fukanchik@postgrespro.ru>
2025-08-28 17:36:04 +0300

    Sometimes golang slice does not modify underlying array

--- golang/slice-modify-underlying.go
+++ golang/slice-modify-underlying.go
@@ -1,4 +1,7 @@
-/* Appending to a slice modifies underlying array! */
+/*
+ * 1. Appending to a slice modifies underlying array
+ * 2. But sometimes not
+ */
 package main
 
 import (
@@ -6,8 +9,14 @@ import (
 )
 
 func main() {
+    // Appending to a slice modifies underlying array
 	slice := []int{0, 0, 0};
 	newSlice := slice[0:2]
 	newSlice = append(newSlice, 1)
-	fmt.Println(slice)
+	fmt.Println("Original, but modified", slice, " should be copy but the same", newSlice)
+
+    // But sometimes not
+    sliceCopy := slice[0:2]
+    sliceCopy = append(sliceCopy, 1, 2, 3, 4, 5)
+    fmt.Println("Original, not modified", slice, " copy ", sliceCopy)
 }
