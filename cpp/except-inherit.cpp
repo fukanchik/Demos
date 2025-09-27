74c530a2774066b3cf9067bdd684282b7f1ce9b4
Sergey Fukanchik <s.fukanchik@postgrespro.ru>
2025-09-27 08:54:27 +0300

    Try virtual

--- cpp/except-inherit.cpp
+++ cpp/except-inherit.cpp
@@ -4,6 +4,7 @@
 struct e1 : std::exception {};
 struct e2 : std::exception {};
 struct e3 : e1, e2 {};
+struct e4 : virtual e1, e2 {};
 
 int main() {
   try {
@@ -12,5 +13,13 @@ int main() {
     std::cerr << "Handler 1" << std::endl;
   } catch (...) {
     std::cerr << "Handler 2" << std::endl;
+  }
+
+  try {
+	  throw e4();
+  } catch (std::exception &e) {
+    std::cerr << "Handler 3" << std::endl;
+  } catch (...) {
+    std::cerr << "Handler 4" << std::endl;
   }
 }
